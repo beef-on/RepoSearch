fbd6f1000d96f25a7c37abba5344ee43228e50b2
Brandon Fong <brandyfongfong@berkeley.edu>
2019-11-04 10:04:37 -0800

    Implemented tokenization and search.

--- searcher.py
+++ searcher.py
@@ -1,12 +1,16 @@
+import nltk
 from github import Github
+from nltk.tokenize import word_tokenize
 
 class Searcher:
 
-    def __init__(self, username = None, password = None, access_token = None, base_url = None):
+    def __init__(self, username=None, password=None,
+                 access_token=None, base_url=None):
         if username and password:
             self.github = Github(username, password)
         elif access_token and base_url:
-            self.github = Github(base_url = base_url, login_or_token = access_token)
+            self.github = Github(base_url=base_url,
+                                 login_or_token=access_token)
         elif access_token:
             self.github = Github(access_token)
 
@@ -14,10 +18,20 @@ class Searcher:
 
     def set_repository(self, repo):
         self.repo = self.github.get_repo(repo)
+        self.create_tfidf_model()
 
-    def search(self, input_path):
-        with open(input_path) as input_file:
-
+    def create_tfidf_model(self):
+        self.contents = self.contents()
+        self.num_contents = len(self.contents)
+        content_tokens = [[w.lower() for w in word_tokenize(c.decoded_contents)] \
+                            for c in content]
+        self.dictionary = gensim.corpora.Dictionary(tokens)
+        corpus = [self.dictionary.doc2bow(tokens) \
+                    for tokens in content_tokens]
+        self.tf_idf = gensim.models.TfidfModel(corpus)
+        self.sim = gensim.similarities.Similarity('sim/',
+                    self.tf_idf[self.corpus],
+                    num_features = len(self.dictionary))
 
     def contents(self):
         if not repo:
@@ -25,9 +39,22 @@ class Searcher:
             return
 
         contents = repo.get_contents("")
-        while contents:
-            file_or_dir = contents.pop(0)
+        for file_or_dir in contents:
             if file_or_dir.type = "dir":
-                contents.extend(repo.get_contents(file_or_dir.path))    
-            else:
-                yield file_or_dir
+                contents.extend(repo.get_contents(file_or_dir.path))
+
+        return contents
+
+    def search(self, query_path, file_limit=float("inf")):
+        if not repo:
+            print("No repository is currently set for search.")
+            return
+
+        with open(query_path) as query_file:
+            query_tokens = [w.lower() for w in word_tokenize(query_file.read())]
+            query_bow = self.dictionary.doc2bow(query_tokens)
+            similarities = self.sim(self.tf_idf(query_bow))
+
+            index_and_score = sorted(enumerate(similarities),
+                                key=lambda x: x[1])[:file_limit]
+            return [(self.contents[index], score) for index, score in index_and_score]
